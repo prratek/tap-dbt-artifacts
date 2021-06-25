ebd317b10f7be8cdf65ccf521f6d2390cfb74243
prratek <prratek.95@gmail.com>
2021-06-25 09:15:39 -0400

    write test to check all streams are discoverable

--- tap_dbt_artifacts/tests/test_core.py
+++ tap_dbt_artifacts/tests/test_core.py
@@ -2,11 +2,13 @@
 
 import os
 
+from singer_sdk.helpers._util import read_json_file
+from singer_sdk.tap_base import Tap
 from singer_sdk.testing import get_standard_tap_tests
 
 from tap_dbt_artifacts.tap import TapDbtArtifacts
 
-SAMPLE_CONFIG = {"dbt_target_dir": os.environ.get("DBT_TARGET_DIR")}
+SAMPLE_CONFIG = read_json_file(".secrets/config.json")
 
 
 # Run standard built-in tap tests from the SDK:
@@ -17,4 +19,8 @@ def test_standard_tap_tests():
         test()
 
 
-# TODO: Create additional tests as appropriate for your tap.
+def test_discover_stream_output():
+    tap: Tap = TapDbtArtifacts(config=SAMPLE_CONFIG, parse_env_config=True)
+    streams = tap.discover_streams()
+    stream_ids = {stream.tap_stream_id for stream in streams}
+    assert stream_ids == {"manifest", "catalog", "sources", "run_results"}
