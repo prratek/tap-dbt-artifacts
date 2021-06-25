19773e6b3789ae1e7a1b839552fc0f429b44cda2
prratek <prratek.95@gmail.com>
2021-06-24 23:21:20 -0400

    format with black

--- tap_dbt_artifacts/streams.py
+++ tap_dbt_artifacts/streams.py
@@ -5,7 +5,6 @@ from typing import Dict, List
 
 from tap_dbt_artifacts.client import DbtArtifactsStream
 
-
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
@@ -16,6 +15,7 @@ def create_id_col(record: dict, col_name: str = "id") -> dict:
 
 class CatalogStream(DbtArtifactsStream):
     """Stream for manifest.json"""
+
     name = "catalog"
     primary_keys = ["id"]
     replication_key = None
@@ -32,7 +32,9 @@ class CatalogStream(DbtArtifactsStream):
         record = create_id_col(record)
 
         for field in fields_to_listify:
-            record[field] = [self._listify_columns(record[field][entry]) for entry in record[field]]
+            record[field] = [
+                self._listify_columns(record[field][entry]) for entry in record[field]
+            ]
             if field == "nodes":
                 record[field] = [entry for entry in record[field]]
 
@@ -41,6 +43,7 @@ class CatalogStream(DbtArtifactsStream):
 
 class ManifestStream(DbtArtifactsStream):
     """Stream for manifest.json"""
+
     name = "manifest"
     primary_keys = ["id"]
     replication_key = None
@@ -54,20 +57,30 @@ class ManifestStream(DbtArtifactsStream):
 
     @staticmethod
     def _listify_cluster_by(node: dict) -> dict:
-        if node.get("config", {}).get("cluster_by") and isinstance(node["config"]["cluster_by"], str):
+        if node.get("config", {}).get("cluster_by") and isinstance(
+            node["config"]["cluster_by"], str
+        ):
             node["config"]["cluster_by"] = [node["config"]["cluster_by"]]
-        if node.get("unrendered_config", {}).get("cluster_by") and isinstance(node["unrendered_config"]["cluster_by"], str):
-            node["unrendered_config"]["cluster_by"] = [node["unrendered_config"]["cluster_by"]]
+        if node.get("unrendered_config", {}).get("cluster_by") and isinstance(
+            node["unrendered_config"]["cluster_by"], str
+        ):
+            node["unrendered_config"]["cluster_by"] = [
+                node["unrendered_config"]["cluster_by"]
+            ]
         return node
 
     @staticmethod
     def _stringify_accepted_values(node: dict) -> dict:
         if node.get("test_metadata", {}).get("kwargs", {}).get("values"):
-            node["test_metadata"]["kwargs"]["values"] = [str(val) for val in node["test_metadata"]["kwargs"]["values"]]
+            node["test_metadata"]["kwargs"]["values"] = [
+                str(val) for val in node["test_metadata"]["kwargs"]["values"]
+            ]
         return node
 
     @staticmethod
-    def _wrap_nested_array(nested_arr: List[List], key_name: str) -> List[Dict[str, List]]:
+    def _wrap_nested_array(
+        nested_arr: List[List], key_name: str
+    ) -> List[Dict[str, List]]:
         return [{key_name: val} for val in nested_arr]
 
     def _wrap_node_nested_arrays(self, node: dict) -> dict:
@@ -78,30 +91,43 @@ class ManifestStream(DbtArtifactsStream):
         return node
 
     def process_record(self, record: dict) -> dict:
-        fields_to_listify = ["nodes", "sources", "macros", "docs", "exposures", "selectors"]
+        fields_to_listify = [
+            "nodes",
+            "sources",
+            "macros",
+            "docs",
+            "exposures",
+            "selectors",
+        ]
         parent_child_fields = ["parent_map", "child_map"]
         record = create_id_col(record)
 
         for field in fields_to_listify:
-            record[field] = [self._listify_columns(record[field][entry]) for entry in record[field]]
+            record[field] = [
+                self._listify_columns(record[field][entry]) for entry in record[field]
+            ]
             if field == "nodes":
                 record[field] = [
-                    self._stringify_accepted_values(self._listify_cluster_by(self._wrap_node_nested_arrays(entry)))
+                    self._stringify_accepted_values(
+                        self._listify_cluster_by(self._wrap_node_nested_arrays(entry))
+                    )
                     for entry in record[field]
                 ]
             elif field == "exposures":
-                record[field] = [self._wrap_node_nested_arrays(entry) for entry in record[field]]
+                record[field] = [
+                    self._wrap_node_nested_arrays(entry) for entry in record[field]
+                ]
 
         for field in parent_child_fields:
             record[field] = [
-                {"node": node, "parents": record[field][node]}
-                for node in record[field]
+                {"node": node, "parents": record[field][node]} for node in record[field]
             ]
         return record
 
 
 class RunResultsStream(DbtArtifactsStream):
     """Stream for manifest.json"""
+
     name = "run_results"
     primary_keys = ["id"]
     replication_key = None
@@ -114,12 +140,15 @@ class RunResultsStream(DbtArtifactsStream):
 
     def process_record(self, record: dict) -> dict:
         record = create_id_col(record)
-        record["results"] = [self._stringify_message(result) for result in record["results"]]
+        record["results"] = [
+            self._stringify_message(result) for result in record["results"]
+        ]
         return record
 
 
 class SourcesStream(DbtArtifactsStream):
     """Stream for manifest.json"""
+
     name = "sources"
     primary_keys = ["id"]
     replication_key = None
--- tap_dbt_artifacts/tests/test_core.py
+++ tap_dbt_artifacts/tests/test_core.py
@@ -6,18 +6,13 @@ from singer_sdk.testing import get_standard_tap_tests
 
 from tap_dbt_artifacts.tap import TapDbtArtifacts
 
-SAMPLE_CONFIG = {
-    "dbt_target_dir": os.environ.get("DBT_TARGET_DIR")
-}
+SAMPLE_CONFIG = {"dbt_target_dir": os.environ.get("DBT_TARGET_DIR")}
 
 
 # Run standard built-in tap tests from the SDK:
 def test_standard_tap_tests():
     """Run standard tap tests from the SDK."""
-    tests = get_standard_tap_tests(
-        TapDbtArtifacts,
-        config=SAMPLE_CONFIG
-    )
+    tests = get_standard_tap_tests(TapDbtArtifacts, config=SAMPLE_CONFIG)
     for test in tests:
         test()
 
